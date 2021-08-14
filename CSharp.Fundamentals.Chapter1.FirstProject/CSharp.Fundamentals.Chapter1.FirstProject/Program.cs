f803d30faa23d162abdb0ac39fbffb303a4eae80
dinabogdan <bogdandina@microsoft.com>
2021-08-14 23:44:47 +0300

    Chapter 1 - Hello World project example

---
+++ CSharp.Fundamentals.Chapter1.FirstProject/CSharp.Fundamentals.Chapter1.FirstProject/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CSharp.Fundamentals.Chapter1.FirstProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
